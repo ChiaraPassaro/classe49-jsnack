0c785a51dcfa6f115c377b53b7408822ae11cd26
Chiara Passaro <chiarapassaro.wd@gmail.com>
2021-11-29 12:57:57 +0100

    snack

--- jsnack1.js
+++ jsnack1.js
@@ -7,11 +7,60 @@
 // console.log(sum);
 
 
-let sum = 0;
-let i = 0;
+// let sum = 0;
+// let i = 0;
 
-while (i < 10) {
-  const number = parseInt(prompt('inserisci un numero'));
-  sum += number;
+// while (i < 10) {
+//   const number = parseInt(prompt('inserisci un numero'));
+//   sum += number;
+//   i++;
+// }
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const guests = [
+  'Mario',
+  'Luisa',
+  'Arturo',
+  'Sofia',
+  'Giuseppe',
+  'Filippo',
+  'Manuela',
+  'Valerio',
+  'Edoardo'
+];
+
+//chiediamo all'utente il suo nome
+const nameUser = prompt('Inserisci il tuo nome');
+
+// let message = 'Non puoi entrare';
+// //controlliamo se nell'array abbiamo una corrispondenza
+// for (let index = 0; index < guests.length; index++) {
+//   //guests[1]
+//   console.log(guests[index]);
+//   if (nameUser == guests[index]) {
+//     console.log('Trovato');
+//     message = 'Puoi entrare';
+//   }
+// }
+
+// console.log(message);
+
+//nameUser != guests[i]
+//find false
+//i < guests.length
+
+let find = false;
+let i = 0;
+//find == false  &&  i < guests.length // vero solo se entrambe vere
+while (find == false && i < guests.length) {
+  console.log(guests[i]);
+  if (nameUser == guests[i]) {
+    console.log('Trovato');
+    find = true;
+  }
   i++;
 }
+
+console.log('find ', find);
